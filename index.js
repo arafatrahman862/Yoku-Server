8b2c7fc71b28112528c1edfe5c6a86a810cb5ec7
xtr <99972602+arafatrahman862@users.noreply.github.com>
2023-06-22 01:44:42 +0600

    Yoku-Server

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+import { } from "./src/database.js";
+import { } from "./src/auth.js";
+import { PORT } from "./src/config.js";
+import { errorHandler } from "./src/error.js";
+
+import classRouter from "./routes/class.js";
+import authRouter from "./routes/auth.js";
+import studentRouter from "./routes/student.js";
+import instructorRouter from "./routes/instructor.js";
+
+const app = express();
+
+app.listen(PORT, () => console.info(`listening on port ${PORT}`));
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/class", classRouter);
+app.use("/auth", authRouter);
+app.use("/student", studentRouter);
+app.use("/instructor", instructorRouter);
+
+// catch 404 and forward to `errorHandler`
+app.use((_req, _res, next) => next(404));
+app.use(errorHandler);
